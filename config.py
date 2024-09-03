60f5fce5778bcf7e1107e28e0d62284fe8cf5b43
82mahdi82 <141176041+82mahdi82@users.noreply.github.com>
2024-09-03 12:14:26 +0330

    edit config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 LogFile = 'bot.log'
 Local = True
-config={'user': 'root', 'password': 'n3gKJd4CiFTnovf9VpOb', 'host': 'amirzadata-rtc-service','database':'amirzadadlo_db'}
+config={'user': 'root', 'password': 'n3gKJd4CiFTnovf9VpOb', 'host': 'amirzadadlo_db','database':'amirzadata-rtc-service'}
